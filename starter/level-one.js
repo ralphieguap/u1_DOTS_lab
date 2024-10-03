21e4600e0c763e726eb5b7f55335096f17a68315
Ralphieguap <ralphie.guappone@gmail.com>
2024-10-02 22:20:00 -0400

    submit

--- starter/level-one.js
+++ starter/level-one.js
@@ -1,4 +1,17 @@
-/*
- * DOTS: Level One
- *
- */
+let score = 0
+let arenaEl = document.querySelector('.js-arena')
+let scoreEl = document.querySelector('.js-score')
+
+ballEl.addEventListener('click', () => {
+  score += 10
+
+  if ( score < 100 ) {
+    scoreEl.innerText = score
+  } else {
+    declareWinner()
+  }
+})
+
+function declareWinner() {
+  document.body.classList.add('game-over')
+}
